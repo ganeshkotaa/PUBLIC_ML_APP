70779112d5c8ef085c0221e90c5b082cbf1b732a
ganeshkotaa <150371568+ganeshkotaa@users.noreply.github.com>
2024-07-23 23:55:55 +0530

    combined.py

--- combined.py
+++ combined.py
@@ -1,42 +1,23 @@
 
 # -*- coding: utf-8 -*-
 
-
 import pickle
 import streamlit as st
-from streamlit_option_menu import option_menu
-
 
 # loading the saved models
-
 diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
-
 heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
-
 parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
+# Sidebar for navigation
+menu_options = ["Diabetes Prediction", "Heart Disease Prediction", "Parkinsons Prediction"]
+selected = st.sidebar.radio("Multiple Disease Prediction System", menu_options)
 
-
-# sidebar for navigation
-with st.sidebar:
-    
-    selected = option_menu('Multiple Disease Prediction System',
-                          
-                          ['Diabetes Prediction',
-                           'Heart Disease Prediction',
-                           'Parkinsons Prediction'],
-                          icons=['activity','heart','person'],
-                          default_index=0)
-    
-    
 # Diabetes Prediction Page
-if (selected == 'Diabetes Prediction'):
-    
-    # page title
+if selected == 'Diabetes Prediction':
     st.title('Diabetes Prediction using ML')
     
-    
-    # getting the input data from the user
+    # Getting the input data from the user
     col1, col2, col3 = st.columns(3)
     
     with col1:
@@ -63,29 +44,22 @@ if (selected == 'Diabetes Prediction'):
     with col2:
         Age = st.text_input('Age of the Person')
     
-    
-    # code for Prediction
+    # Code for Prediction
     diab_diagnosis = ''
     
-    # creating a button for Prediction
-    
+    # Creating a button for Prediction
     if st.button('Diabetes Test Result'):
         diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
         
-        if (diab_prediction[0] == 1):
-          diab_diagnosis = 'The person is diabetic'
+        if diab_prediction[0] == 1:
+            diab_diagnosis = 'The person is diabetic'
         else:
-          diab_diagnosis = 'The person is not diabetic'
+            diab_diagnosis = 'The person is not diabetic'
         
     st.success(diab_diagnosis)
 
-
-
-
 # Heart Disease Prediction Page
-if (selected == 'Heart Disease Prediction'):
-    
-    # page title
+if selected == 'Heart Disease Prediction':
     st.title('Heart Disease Prediction using ML')
     
     col1, col2, col3 = st.columns(3)
@@ -128,32 +102,23 @@ if (selected == 'Heart Disease Prediction'):
         
     with col1:
         thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
-        
-        
-     
-     
-    # code for Prediction
+    
+    # Code for Prediction
     heart_diagnosis = ''
     
-    # creating a button for Prediction
-    
+    # Creating a button for Prediction
     if st.button('Heart Disease Test Result'):
-        heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg,thalach,exang,oldpeak,slope,ca,thal]])                          
+        heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])                          
         
-        if (heart_prediction[0] == 1):
-          heart_diagnosis = 'The person is having heart disease'
+        if heart_prediction[0] == 1:
+            heart_diagnosis = 'The person is having heart disease'
         else:
-          heart_diagnosis = 'The person does not have any heart disease'
+            heart_diagnosis = 'The person does not have any heart disease'
         
     st.success(heart_diagnosis)
-        
-    
-    
 
 # Parkinson's Prediction Page
-if (selected == "Parkinsons Prediction"):
-    
-    # page title
+if selected == "Parkinsons Prediction":
     st.title("Parkinson's Disease Prediction using ML")
     
     col1, col2, col3, col4, col5 = st.columns(5)  
@@ -223,23 +188,20 @@ if (selected == "Parkinsons Prediction"):
         
     with col2:
         PPE = st.text_input('PPE')
-        
     
-    
-    # code for Prediction
+    # Code for Prediction
     parkinsons_diagnosis = ''
     
-    # creating a button for Prediction    
+    # Creating a button for Prediction    
     if st.button("Parkinson's Test Result"):
-        parkinsons_prediction = parkinsons_model.predict([[fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ,DDP,Shimmer,Shimmer_dB,APQ3,APQ5,APQ,DDA,NHR,HNR,RPDE,DFA,spread1,spread2,D2,PPE]])                          
+        parkinsons_prediction = parkinsons_model.predict([[fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ, DDP, Shimmer, Shimmer_dB, APQ3, APQ5, APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]])                          
         
-        if (parkinsons_prediction[0] == 1):
-          parkinsons_diagnosis = "The person has Parkinson's disease"
+        if parkinsons_prediction[0] == 1:
+            parkinsons_diagnosis = "The person has Parkinson's disease"
         else:
-          parkinsons_diagnosis = "The person does not have Parkinson's disease"
+            parkinsons_diagnosis = "The person does not have Parkinson's disease"
         
     st.success(parkinsons_diagnosis)
-
 
 
 
